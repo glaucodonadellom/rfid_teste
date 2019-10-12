a3f259a8f02299798d58abf426afb8caf7d388e8
Glauco Donadello <glaucodonadellom@gmail.com>
2019-10-12 18:49:32 -0300

    Retirando relatorios

--- enviarBancoPresenca.php
+++ enviarBancoPresenca.php
@@ -11,17 +11,17 @@ foreach ($dados as $linha){
     $linha= trim($linha);
     $data = substr($linha, 5, 16);
     $card = substr($linha, 22, 15);
-    echo $data;
-    echo"<br>";
-    echo $card;
-    echo"<br>";
+//    echo $data; //Mostra a data cadastrada no TXT
+//    echo"<br>";
+//    echo $card;// Mostra o numero do card cadastrado no TXT
+//    echo"<br>";
     $comandoSQL = "SELECT `idCard`,`idALuno_FK` FROM `card` WHERE `card`=$card";
     $rs = mysqli_query($con, $comandoSQL) or die ("Erro " . mysqli_error($con));
     $linhas = mysqli_num_rows($rs);
 //    $dado = mysqli_fetch_array($rs);
 
     if ($linhas < 1) {
-        echo "SEM ID";
+//        echo "SEM ID";// Se não existir o ID ele cadastrará no ID 1 - sem cadastro
         $comandoSQL = "INSERT INTO card(card,idAluno_FK)values ('$card','1')";
         mysqli_query($con, $comandoSQL)
         or die("Erro na inclusão do cartão: $card<br>" .
@@ -34,27 +34,25 @@ foreach ($dados as $linha){
             mysqli_error($con) );
 
         }
-    var_dump($rs);
+
     $dado = mysqli_fetch_array($rs);
     $idAluno_fk = $dado['idALuno_FK'];
     $idCard_fk = $dado['idCard'];
-    var_dump($idAluno_fk);
-    var_dump($idCard_fk);
     $comandoSQL = "INSERT INTO presenca (`datapresenca`,`idCard_fk`,`idAluno_fk`)values ('$data','$idCard_fk','$idAluno_fk');";
-    echo "COM ID ALUNO";
-
-    var_dump($comandoSQL);
     mysqli_query($con, $comandoSQL)
     or die("Erro na inclusão da presença: $card<br>" .
         mysqli_error($con) );
 }
 echo "Inclusão de presença concluido com sucesso"
-
+?>
+</form>
+<form method="post" action="index.php">
+  <input type="submit" value="Voltar"  maxlength="50">
+</form>
+<?php
 //se tiver "data" no arquivo //
 //$data = substr($dados, 5, 16);
 //$card = substr($dados, 22, 10);
-
-
 
 
 //se nao tiver "data" no arquivo
